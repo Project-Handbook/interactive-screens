9f85ba264af3a636aad7ed933ec730ea7b8de3f8
Jonas Johansson <jonassjoh@hotmail.com>
2016-02-28 00:53:28 +0100

    ihasdoh
    aioshd

--- app/find-person/find-person.ts
+++ app/find-person/find-person.ts
@@ -1,9 +1,17 @@
 import { Component } from 'angular2/core';
+import {NgClass} from 'angular2/common';
 
 @Component({
   selector: 'find-person',
-  templateUrl: 'app/find-person/partial_find-person.html'
+  templateUrl: 'app/find-person/partial_find-person.html',
+  directives: [NgClass]
 })
 export class FindPerson {
-
+	isOn = false;
+  	isDisabled = false;
+  	toggle(newState) {
+    if (!this.isDisabled) {
+      	this.isOn = newState;
+    }
+  }
 }
