afa0bb0f377d2aa5047ab70280d78a48cf997866
Jonas Johansson <jonassjoh@hotmail.com>
2016-02-28 20:03:53 +0100

    dgsffgddfg
    dfgdfgdfggdf

--- app/find-person/find-person.service.ts
+++ app/find-person/find-person.service.ts
@@ -11,15 +11,37 @@ export class FindPersonService{
 	answer: Array<Object>=[];
 
 	getPeople(term: string): Array<Object> {
+		this.answer = [];
 		var url = "https://www.lan.kth.se/personal/api/katalogjson?q=";
 		this._http.get(url + term)
 			.map(res=> res.json())
 			.subscribe(res=> {
-				res.result.forEach(item=> { this.answer.push(item) })
+				res.result.forEach(item=> {
+					this.getImage(item);
+				})
 			},
 				error=> console.log(error),
-				() => console.log('OK People')
-			)
+				() => {
+					console.log('OK People');
+				}
+			);
 		return this.answer;
+	}
+	result : string;
+	getImage(a : Object) {
+		var url = "https://www.kth.se/social/api/profile/1.1/" + a.kthid + "/image";
+		this._http.get(url).map(res=> res.text()).subscribe(
+			data => {
+				a.image = data.substr(1, data.length - 2);
+				this.answer.push(a);
+			},
+			error => console.log(error),
+			() => {
+				// console.log('OK Images');
+			}
+		);
+	}
+	getImages() : Array<string> {
+		return this.images;
 	}
 }
--- app/find-person/find-person.ts
+++ app/find-person/find-person.ts
@@ -1,10 +1,12 @@
 import { Component } from 'angular2/core';
 import {NgClass} from 'angular2/common';
+import {FindPersonService} from './find-person.service';
 
 @Component({
   selector: 'find-person',
   templateUrl: 'app/find-person/partial_find-person.html',
-  directives: [NgClass]
+  directives: [NgClass],
+  providers: [FindPersonService]
 })
 export class FindPerson {
 	isOn = false;
@@ -13,5 +15,19 @@ export class FindPerson {
     if (!this.isDisabled) {
       	this.isOn = newState;
     }
+  }
+  staben: string = "org:DAS";
+  people: Array<Object>=[];
+  constructor(private _findPersonService:FindPersonService){
+  }
+  getPeople(term:string){
+    console.log(this.people);
+    this.people = [];
+    this.people = this._findPersonService.getPeople(term);
+    this.peopleImage = this._findPersonService.getImages();
+  }
+  getTitle(title : string) : string {
+    var res = title.charAt(0) + title.substr(1).toLowerCase();
+    return res;
   }
 }
