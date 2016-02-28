283bdfa852b271f4140b4767eb5996a830777025
emilppp <emiper@kth.se>
2016-02-28 20:19:27 +0100

    New fixes

--- app/contact/contact.ts
+++ app/contact/contact.ts
@@ -1,9 +1,31 @@
-import { Component } from 'angular2/core';
+import {Component} from 'angular2/core';
+//import { EmailService } from './email.service.ts';
 
 @Component({
   selector: 'contact',
-  templateUrl: 'app/contact/partial_contact.html'
+  templateUrl: 'app/contact/partial_contact.html',
+//  providers: [EmailService],
+
 })
 export class Contact {
+  public email = {message: "", reciever: ""};
+  public subject = "General contact";
 
+
+  onSubmit(reciever, subject, message) {
+    console.log("Mail to: " + reciever + "\n Subject: " + subject + "\nMessage: " + message);
+  }
+
+  changeSubject(subject){
+    if (subject === 1) {
+      this.subject = "Booking"
+    }
+    else if (subject === 2) {
+      this.subject = "Error report"
+    }
+    else {
+      this.subject = "General contact"
+    }
+
+  }
 }
