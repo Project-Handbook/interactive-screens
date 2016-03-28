c3c9e864d81dcb32161f6c0cf0e627acbdcb7dc1
Alexander Lingtorp <AlexanderLingtorp@Alexanders-MacBook-Pro.local>
2016-03-28 22:05:26 +0200

    [Home screen]: Added basic implementation

--- app/home/home.ts
+++ app/home/home.ts
@@ -1,9 +1,16 @@
 import { Component } from 'angular2/core';
+import { Http, HTTP_PROVIDERS } from 'angular2/http';
 
 @Component({
   selector: 'home',
-  template: '<h1>Home component here</h1>'
+  viewProviders: [HTTP_PROVIDERS],
+  templateUrl: 'app/home/home.html'
 })
 export class Home {
+    news_block: String
 
+    constructor(http: Http) {
+      http.get("https://www.kth.se/cm/1.314503")
+        .subscribe(res => this.news_block = res.text())
+    }
 }
