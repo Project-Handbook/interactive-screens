8d42811ae9746379dcf4f60a1994490b069d7eea
jakobsvenningsson <jaksve@kth.se>
2016-03-30 15:32:12 +0200

    Added service class and search field

--- app/map/map-service.ts
+++ app/map/map-service.ts
@@ -1,6 +1,12 @@
 import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+import "rxjs/add/operator/map";
 @Injectable()
 
 export class MapService{
-
+	constructor(private _http:Http){}
+	getPlaces(term:string){
+		var url = "https://www.kth.se/api/places/v3/search/room?q=" + term + "&api_key=lkjashd(%26*0987-7-0Ujuhdhj4HGRESDs";
+	 	return this._http.get(url).map(res => res.json());
+	}
 }
--- app/map/map.ts
+++ app/map/map.ts
@@ -2,9 +2,25 @@ import { Component } from 'angular2/core';
 import {MapService} from './map-service';
 @Component({
   selector: 'map',
-  template: '<h1>Map component here</h1>',
+  template: `
+		<h1>Map component here</h1>
+		<form action="search()" id="search-container">
+				<input type="text" [(ngModel)]="input" id="person-search-input" placeholder="Search for someone...">
+				<button type="submit" (click)="search(input)" name="search_button" >Search</button>
+		</form>
+		<ul><li *ngFor="#res of searchResult">{{res.kthLokalkod}}</li></ul>	
+  `,
   providers:[MapService],
 })
 export class Map {
+	searchResult:Array<Object>;
+
+	constructor(private _mapService:MapService){}
+	search(term:string){
+		this.searchResult = [];
+		this._mapService.getPlaces(term)
+			.subscribe(res => { this.searchResult = res ,console.log(res)},error=>console.log(error),()=>console.log("hej"));
+	}
 
 }
+//		<ul><li *ngFor="#res of searchResult">{{res}}</li></ul>
