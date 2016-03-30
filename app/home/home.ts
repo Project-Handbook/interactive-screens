2c0edc0534f5bfc11ffa4116bfb3d4e6334cab03
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-03-30 10:52:48 +0200

    Started implementation of Home screen.

--- app/home/home.ts
+++ app/home/home.ts
@@ -7,10 +7,28 @@ import { Http, HTTP_PROVIDERS } from 'angular2/http';
   templateUrl: 'app/home/home.html'
 })
 export class Home {
+    private base_polopoly_url = "https://www.kth.se/cm/"
+
+    private calendar_polopoly_id = "1.231565"
+    private news_polopoly_id = "1.314503"
+
+    /**
+    * Description to be added
+    * @property {String} news_block
+    */
     news_block: String
 
+    /**
+    * Description to be added
+    * @property {String} news_block
+    */
+    calendar_block: String
+
     constructor(http: Http) {
-      http.get("https://www.kth.se/cm/1.314503")
+      http.get(this.base_polopoly_url + this.news_polopoly_id)
         .subscribe(res => this.news_block = res.text())
+
+      http.get(this.base_polopoly_url + this.calendar_polopoly_id)
+        .subscribe(res => this.calendar_block = res.text())
     }
 }
