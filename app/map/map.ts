7a19e2d0e7c1a51c4aba5cc50a1fa9becc89879c
jakobsvenningsson <jaksve@kth.se>
2016-03-30 13:46:05 +0200

    Init commit of map feature

--- app/map/map.ts
+++ app/map/map.ts
@@ -1,8 +1,9 @@
 import { Component } from 'angular2/core';
-
+import {MapService} from './map-service';
 @Component({
   selector: 'map',
-  template: '<h1>Map component here</h1>'
+  template: '<h1>Map component here</h1>',
+  providers:[MapService],
 })
 export class Map {
 
