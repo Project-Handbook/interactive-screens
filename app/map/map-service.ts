98e3a039b9bca9edb620e7206a175b7d013e5892
jakobsvenningsson <jaksve@kth.se>
2016-04-05 16:12:49 +0200

    Added leaflet map, added leaflet dependecy in gruntFile, Location interface

--- app/map/map-service.ts
+++ app/map/map-service.ts
@@ -5,6 +5,7 @@ import "rxjs/add/operator/map";
 
 export class MapService{
 	constructor(private _http:Http){}
+	//Fetches all locations from KTH Places that matches the given search string.
 	getPlaces(term:string){
 		var url = "https://www.kth.se/api/places/v3/search/room?q=" + term + "&api_key=lkjashd(%26*0987-7-0Ujuhdhj4HGRESDs";
 	 	return this._http.get(url).map(res => res.json());
--- app/map/map.ts
+++ app/map/map.ts
@@ -1,26 +1,76 @@
 import { Component } from 'angular2/core';
 import {MapService} from './map-service';
+import {Location} from './location.interface';
+/// <reference path="../../typings/leaflet/leaflet.d.ts"/>
+
 @Component({
   selector: 'map',
-  template: `
-		<h1>Map component here</h1>
-		<form action="search()" id="search-container">
-				<input type="text" [(ngModel)]="input" id="person-search-input" placeholder="Search for someone...">
-				<button type="submit" (click)="search(input)" name="search_button" >Search</button>
-		</form>
-		<ul><li *ngFor="#res of searchResult">{{res.kthLokalkod}}</li></ul>	
-  `,
-  providers:[MapService],
+  templateUrl:'./app/map/map.html',
+  styleUrls:['./app/map/map.min.css'],  
+  providers:[MapService]
 })
 export class Map {
-	searchResult:Array<Object>;
+  //Leaflet Map Object
+  map: L.Map;
+  //Leaflet Marker Object
+  currentDestination: L.Marker;
 
+  searchResult:Array<Location>; 
 	constructor(private _mapService:MapService){}
+  //Fetches Places that matches the given search term. Only returns places of type Övningssal and Datorsal at the moment.
 	search(term:string){
 		this.searchResult = [];
 		this._mapService.getPlaces(term)
-			.subscribe(res => { this.searchResult = res ,console.log(res)},error=>console.log(error),()=>console.log("hej"));
+			.subscribe(res => { console.log(res),
+        res.forEach(item=>{
+          if ((item.typeName === "Övningssal" || item.typeName === "Datorsal" )&& item.kthLokalkod.length !== 0) {
+             this.searchResult.push(
+                {
+                  latitude:item.geoData.lat,
+                  longitude:item.geoData.long,
+                  buildingName:item.buildingName,
+                  roomCode:item.kthLokalkod,
+                  streetAddress:item.streetAddress,
+                  streetNumber:item.streetNumber,
+                  roomType:item.typeName,
+                  zipCode:item.zip,
+                  floor:item.floor
+                }
+              );
+             } 
+           }
+        )
+      },
+      error=>console.log(error),
+      ()=>console.log("Success"));
 	}
+  //Executes on page load.
+	ngOnInit(){
+    //Initialize map
+	 this.map = new L.Map('map', {
+            zoomControl: false,
+            center: new L.LatLng(59.3469417, 18.0702413),
+            zoom: 17,
+            minZoom: 4,
+            maxZoom: 18
+        });
+        var baseMap = new L.TileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        	attribution: '&copy; Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+        }).addTo(this.map);
+        var zoomControl = L.control.zoom({
+            position: 'topright'
+        }).addTo(this.map);
+       //Add marker at the location of the screen. 
+			L.marker([59.34694, 18.07319]).addTo(this.map);	 
+  }
+  //Adds a marker on the location the place that the user has searched for. If multiple searches had been made this method
+  //also removed the old destination marker. test
+  addDestinationMarker(place:Location){
+    console.log(place);
+    if (this.currentDestination != null) {
+      this.map.removeLayer(this.currentDestination);
+    } 
+    this.currentDestination = L.marker([place.latitude, place.longitude]).addTo(this.map);
+  }
 
 }
-//		<ul><li *ngFor="#res of searchResult">{{res}}</li></ul>
