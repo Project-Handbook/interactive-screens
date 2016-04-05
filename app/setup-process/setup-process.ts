23491d14f46a444b3a218325ed806f37722adcac
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-04-05 20:32:17 +0200

    Added related files to setup-process.

---
+++ app/setup-process/setup-process.ts
@@ -0,0 +1,12 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'setup-process',
+  templateUrl: 'app/setup-process/setup-process.html'
+})
+export class SetupProcess {
+
+    constructor() {
+
+    }
+}
