8d1ffa9a975049acba260d6b2b9575412832d791
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-04-12 15:49:57 +0200

    Added example to how to handle routerParams in Map.

--- app/map/map.ts
+++ app/map/map.ts
@@ -1,9 +1,14 @@
 import { Component } from 'angular2/core';
+import { RouteParams } from 'angular2/router';
+import { Person } from '../find-person/person';
 
 @Component({
   selector: 'map',
   template: '<h1>Map component here</h1>'
 })
 export class Map {
-
+  constructor(routeParams: RouteParams) {
+    var person = <Person> routeParams.get('person'); // This works (hooray!)
+    console.log(person);
+  }
 }
