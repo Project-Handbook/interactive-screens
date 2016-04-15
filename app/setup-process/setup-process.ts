bf099cd6d7b91c676ed4825e740955ac50fe3a97
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-04-15 08:45:38 +0200

    Added the department handling to the setup class

--- app/setup-process/setup-process.ts
+++ app/setup-process/setup-process.ts
@@ -2,9 +2,11 @@ import { Component } from 'angular2/core';
 import { Router } from 'angular2/router';
 import { Constants } from '../constants';
 import { ScreenSpecificInformation } from '../screen-specific-information';
+import { NgClass } from 'angular2/common';
 
 @Component({
   selector: 'setup-process',
+  directives: [NgClass],
   templateUrl: 'app/setup-process/setup-process.html'
 })
 export class SetupProcess {
@@ -16,6 +18,7 @@ export class SetupProcess {
   // Stores the ScreenSpecificInformation object
   saveInformation() {
     if (!this.validateInputs(this.screenInfo)) { return; }
+    this.screenInfo.departments = this.departments;
     // Save the screen information in the session storage for use by all of the app
     localStorage.setItem(Constants.SETUP_PROCESS_KEY, JSON.stringify(this.screenInfo));
     this.router.navigate(['Home']);
@@ -30,5 +33,16 @@ export class SetupProcess {
   private validateInputs(screenInfo: ScreenSpecificInformation): boolean {
     // TODO: Implement
     return true
+  }
+
+  // Input - need to add this the the list
+  public newDepartment: string = ""
+  // User selected department
+  public selectedDepartment: string = ""
+  // List of user created departments
+  public departments: Array<string> = []
+
+  addDepartment() {
+    this.departments.push(this.newDepartment);
   }
 }
