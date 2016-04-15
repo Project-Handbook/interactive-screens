37888b9c74862f11bd278219c97194a02fd865cf
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-04-15 08:45:07 +0200

    Added more properties to screen info

--- app/screen-specific-information.ts
+++ app/screen-specific-information.ts
@@ -5,5 +5,7 @@ export class ScreenSpecificInformation {
               public address: string = "", /* Address to show in the sidebar */
               public telephoneNumber: string = "", /* Telephone nuber in the sidebar */
               public postal_code: string = "", /* Postal code in the sidebar */
-              public city: string = "") /* Nearest city shown in the sidebar */ {}
+              public city: string = "", /* Nearest city shown in the sidebar */
+              public departments: Array<string> = [], /* List of nearby departments */
+              public nearest_department: string = "") /*  */ {}
 }
