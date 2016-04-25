97132ca99c7d87290e96bb1a8a4998a67e67de57
jonassjoh <jonassjoh@gmail.com>
2016-04-25 22:13:13 +0200

    Modified design sligtly. Prepared school/department list.
    Changed the design of the list of people.
    Made some preparataions for the department list, still needs work
    though. Also it's not functional yet.

--- app/find-person/find-person.ts
+++ app/find-person/find-person.ts
@@ -1,4 +1,4 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 import { OnInit } from 'angular2/core'
 import { NgClass } from 'angular2/common';
 import { ngStyle } from 'angular2/common';
@@ -7,6 +7,9 @@ import { Person } from './person';
 import { PersonProfile } from './person-profile';
 
 @Component({
+  host: {
+        '(document:click)': 'handleClick($event)',
+  },
   selector: 'find-person',
   templateUrl: 'app/find-person/find-person.html',
   directives: [NgClass, PersonProfile],
@@ -61,6 +64,7 @@ export class FindPerson {
   //Displays people local to the department as default when the people tab is pushed.
   ngOnInit(): any {
     this.getPeople(this.organisation);
+    this.getSchools();
   }
 
   search(input: string) {
@@ -161,5 +165,167 @@ export class FindPerson {
       element = title;
     }
     return element;
+  }
+
+  scrollDep(dir) {
+    var newScroll = departments.scrollTop + (departments.offsetHeight-55)*dir;
+
+    console.log(newScroll);
+    this.scrollTo(departments, newScroll, 5000);
+  }
+
+  scrollTo(element, to, duration) {
+    $("#departments").animate({
+            scrollTop: to
+        }, 300);
+        return false;
+  }
+
+  //This funtion determines if the user clicks outside the dropdown menu. If this is the case
+    // the searchresult array will be cleared and the dropdown will disappear.
+    handleClick(event){
+      var clickedComponent = event.target;
+      var inside = false;
+      do {
+         if (clickedComponent === schools 
+            || clickedComponent === departments_wrapper) {
+            inside = true;
+          }
+         clickedComponent = clickedComponent.parentNode;
+      }while (clickedComponent);
+      if(!inside){
+        schools.style.display = "none";
+        this.deps = [];
+      }
+    }
+
+  deps : Array<any> = [];
+  schools : Array<any> = [];
+
+  toggleSchools() {
+    console.log('hello');
+    if(schools.style.display == "block")
+      schools.style.display = "none";
+    else
+      schools.style.display = "block";
+  }
+
+  getSchools() {
+    $("#schools").on("click", function(event){
+      event.stopPropagation();
+      console.log( "I was clicked, but my parent will not be." );
+    });
+
+    this.schools[0] = "KTH";
+    this.schools[1] = "ABC";
+    this.schools[2] = "BIO";
+    this.schools[3] = "CSC";
+    this.schools[4] = "EES";
+    this.schools[5] = "STH";
+    this.schools[6] = "ICT";
+    this.schools[7] = "CHE";
+    this.schools[8] = "ECE";
+    this.schools[9] = "ITM";
+    this.schools[10] = "KHD";
+    this.schools[11] = "UF";
+  }
+
+  getDep() {
+
+    this.deps[0] = "FYSIOKEMISK STRÖMNINGSMEKANIK";
+    this.deps[1] = "FYSIOKEMISK STRÖMNINGSMEKANIK FYSIOKEMISK STRÖMNINGSMEKANIK AAAA";
+    this.deps[2] = "MEKANIK, MIHAESCU";
+    this.deps[3] = "TEORETISK FYSIK";
+    this.deps[4] = "TILLÄMPAD FYSIK GEM";
+    this.deps[5] = "CELLENS FYSIK";
+    this.deps[6] = "KVANTOPTIK";
+    this.deps[7] = "NANOSTRUKTURFYSIK";
+    this.deps[8] = "TILLÄMPAD FYSIK";
+    this.deps[9] = "TILLÄMPAD FYSIK";
+    this.deps[10] = "TILLÄMPAD FYSIK";
+    this.deps[11] = "TILLÄMPAD FYSIK";
+    this.deps[12] = "TILLÄMPAD FYSIK";
+    this.deps[13] = "TILLÄMPAD FYSIK";
+    this.deps[14] = "TILLÄMPAD FYSIK";
+    this.deps[15] = "FYSIOKEMISK STRÖMNINGSMEKANIK FYSIOKEMISK STRÖMNINGSMEKANIK AAAA";
+    this.deps[16] = "MEKANIK, MIHAESCU";
+    this.deps[17] = "TEORETISK FYSIK";
+    this.deps[18] = "TILLÄMPAD FYSIK GEM";
+    this.deps[19] = "CELLENS FYSIK";
+    this.deps[20] = "CELLENS FYSIK";
+    this.deps[21] = "KVANTOPTIK";
+    this.deps[22] = "NANOSTRUKTURFYSIK";
+    this.deps[23] = "TILLÄMPAD FYSIK";
+    this.deps[24] = "TILLÄMPAD FYSIK";
+    this.deps[25] = "TILLÄMPAD FYSIK";
+    this.deps[26] = "TILLÄMPAD FYSIK";
+    this.deps[27] = "TILLÄMPAD FYSIK";
+    this.deps[28] = "TILLÄMPAD FYSIK";
+    this.deps[29] = "FYSIOKEMISK STRÖMNINGSMEKANIK FYSIOKEMISK STRÖMNINGSMEKANIK AAAA";
+    this.deps[30] = "MEKANIK, MIHAESCU";
+    this.deps[31] = "TEORETISK FYSIK";
+    this.deps[32] = "TILLÄMPAD FYSIK GEM";
+    this.deps[33] = "CELLENS FYSIK";
+    this.deps[34] = "KVANTOPTIK";
+    this.deps[35] = "NANOSTRUKTURFYSIK";
+    this.deps[36] = "TILLÄMPAD FYSIK";
+    this.deps[37] = "TILLÄMPAD FYSIK";
+    this.deps[38] = "TILLÄMPAD FYSIK";
+    this.deps[39] = "TILLÄMPAD FYSIK";
+    this.deps[40] = "TILLÄMPAD FYSIK";
+    this.deps[41] = "TILLÄMPAD FYSIK";
+    this.deps[42] = "FYSIOKEMISK STRÖMNINGSMEKANIK FYSIOKEMISK STRÖMNINGSMEKANIK AAAA";
+    this.deps[43] = "MEKANIK, MIHAESCU";
+    this.deps[44] = "TEORETISK FYSIK";
+    this.deps[45] = "TILLÄMPAD FYSIK GEM";
+    this.deps[46] = "CELLENS FYSIK";
+    this.deps[47] = "KVANTOPTIK";
+    this.deps[48] = "NANOSTRUKTURFYSIK";
+    this.deps[49] = "TILLÄMPAD FYSIK";
+    this.deps[50] = "TILLÄMPAD FYSIK";
+    this.deps[51] = "TILLÄMPAD FYSIK";
+    this.deps[52] = "TILLÄMPAD FYSIK";
+    this.deps[53] = "TILLÄMPAD FYSIK";
+    this.deps[54] = "TILLÄMPAD FYSIK";
+    this.deps[55] = "FYSIOKEMISK STRÖMNINGSMEKANIK FYSIOKEMISK STRÖMNINGSMEKANIK AAAA";
+    this.deps[56] = "MEKANIK, MIHAESCU";
+    this.deps[57] = "TEORETISK FYSIK";
+    this.deps[58] = "TILLÄMPAD FYSIK GEM";
+    this.deps[59] = "CELLENS FYSIK";
+    this.deps[60] = "KVANTOPTIK";
+    this.deps[61] = "NANOSTRUKTURFYSIK";
+    this.deps[62] = "TILLÄMPAD FYSIK";
+    this.deps[63] = "TILLÄMPAD FYSIK";
+    this.deps[64] = "TILLÄMPAD FYSIK";
+    this.deps[65] = "TILLÄMPAD FYSIK";
+    this.deps[66] = "TILLÄMPAD FYSIK";
+    this.deps[67] = "TILLÄMPAD FYSIK";
+    this.deps[68] = "FYSIOKEMISK STRÖMNINGSMEKANIK FYSIOKEMISK STRÖMNINGSMEKANIK AAAA";
+    this.deps[69] = "MEKANIK, MIHAESCU";
+    this.deps[71] = "TEORETISK FYSIK";
+    this.deps[72] = "TILLÄMPAD FYSIK GEM";
+    this.deps[73] = "CELLENS FYSIK";
+    this.deps[74] = "KVANTOPTIK";
+    this.deps[75] = "NANOSTRUKTURFYSIK";
+    this.deps[76] = "TILLÄMPAD FYSIK";
+    this.deps[77] = "TILLÄMPAD FYSIK";
+    this.deps[78] = "TILLÄMPAD FYSIK";
+    this.deps[79] = "TILLÄMPAD FYSIK";
+    this.deps[80] = "TILLÄMPAD FYSIK";
+    this.deps[81] = "TILLÄMPAD FYSIK";
+    this.deps[82] = "FYSIOKEMISK STRÖMNINGSMEKANIK FYSIOKEMISK STRÖMNINGSMEKANIK AAAA";
+    this.deps[83] = "MEKANIK, MIHAESCU";
+    this.deps[84] = "TEORETISK FYSIK";
+    this.deps[85] = "TILLÄMPAD FYSIK GEM";
+    this.deps[86] = "CELLENS FYSIK";
+    this.deps[87] = "KVANTOPTIK";
+    this.deps[88] = "KVANTOPTIK";
+    this.deps[89] = "NANOSTRUKTURFYSIK";
+    this.deps[90] = "TILLÄMPAD FYSIK";
+    this.deps[91] = "TILLÄMPAD FYSIK";
+    this.deps[92] = "TILLÄMPAD FYSIK";
+    this.deps[93] = "TILLÄMPAD FYSIK";
+    this.deps[94] = "TILLÄMPAD FYSIK";
+    this.deps[95] = "TILLÄMPAD FYSIK";
   }
 }
