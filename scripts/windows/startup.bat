b4d67bf005f385a9869ef9b75560a2277270035b
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-05-07 18:08:22 +0200

    Added startup batch script for Chrome on Windows.

---
+++ scripts/windows/startup.bat
@@ -0,0 +1,1 @@
+start chrome --overscroll-history-navigation=0 --disable-web-security --user-data-dir --test-type --disable-pinch  -kiosk -fullscreen http://project-handbook.github.io/interactive-screens/
