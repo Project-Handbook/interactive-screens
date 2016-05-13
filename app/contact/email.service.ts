046e062e48d3ff9101645e81a600f53b24715870
Emil Persson <emil.g.persson@gmail.com>
2016-05-13 14:02:30 +0200

    Update email.service.ts

--- app/contact/email.service.ts
+++ app/contact/email.service.ts
@@ -3,19 +3,20 @@ import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class EmailService {
+ 
+ // Initializes an Http object to send a POST request to the Mailgun API
+constructor(private http: Http) {}
 
- constructor(private http: Http) {}
-
- sendEmail(fromName, fromEmail, message) {
-   var headers = new Headers();
-   var recieverName = "CSC Skolan"
-   var recieverMail = "emil.g.persson@gmail.com"
-   var subject = "Errand"
-   headers.append("Authorization", "Basic "+btoa("api:key-5164d1f0b491719c50e103020764205a"));
-   headers.append("content-type", "application/x-www-form-urlencoded");
-   var url = "https://api.mailgun.net/v3/sandbox34e0d52fd37247abab5dbe90b52e2e71.mailgun.org/messages";
-   var data = "from="+fromName+"<"+fromEmail+">&to="+recieverName+"<"+recieverMail+">&subject="+subject+"&text="+message;
-   return this.http.post(url,data, {headers: headers});
-   /* https://api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0@api.mailgun.net/v3/samples.mailgun.org/log */
+// Method for sending the users email. Uses an HTTP-POST request to the Mailgun API with the supplied attributes.
+sendEmail(fromName, fromEmail, message) {
+  var headers = new Headers();
+  var recieverName = "CSC Service"
+  var recieverMail = "service@csc.kth.se"
+  var subject = "Errand"
+  headers.append("Authorization", "Basic "+btoa("api:key-5164d1f0b491719c50e103020764205a"));
+  headers.append("content-type", "application/x-www-form-urlencoded");
+  var url = "https://api.mailgun.net/v3/sandbox34e0d52fd37247abab5dbe90b52e2e71.mailgun.org/messages";
+  var data = "from="+fromName+"<"+fromEmail+">&to="+recieverName+"<"+recieverMail+">&subject="+subject+"&text="+message;
+  return this.http.post(url,data, {headers: headers});
  }
 }
