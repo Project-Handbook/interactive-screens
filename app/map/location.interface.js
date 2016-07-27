4af7c7e7ea3d39212bc791bfb41dc6f943a226fc
jakobsvenningsson <jaksve@kth.se>
2016-07-27 16:33:29 +0200

    Updates

--- app/map/location.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(function (Location_type) {
-    Location_type[Location_type["street_address"] = 0] = "street_address";
-    Location_type[Location_type["kth_places"] = 1] = "kth_places";
-    Location_type[Location_type["department"] = 2] = "department";
-})(exports.Location_type || (exports.Location_type = {}));
-var Location_type = exports.Location_type;
-//# sourceMappingURL=location.interface.js.map
