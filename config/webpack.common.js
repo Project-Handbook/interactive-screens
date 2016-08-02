1ee2fec6a9ff77b77a00e25c3774268cff9a8963
jakobsvenningsson <jaksve@kth.se>
2016-08-02 10:59:54 +0200

    Added webpack config files

---
+++ config/webpack.common.js
@@ -0,0 +1,63 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    'polyfills': './app/polyfills.ts',
+    'vendor': './app/vendor.ts',
+    'app': './app/main.ts'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['ts', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file?name=./assets/[name].[hash].[ext]'
+      },
+      {
+        test:/\.scss$/,
+        exclude: /node_modules/,
+        loader:'style!css!sass'
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        loader: 'raw'
+      },
+      {
+        test:/\.json$/,
+        exclude: /node_modules/,
+        loader:'json'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
